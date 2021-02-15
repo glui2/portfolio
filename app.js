0446ec62e2c19c6e05ae3613d7cadba227650c6d
Gavin <gavin.lui@gmail.com>
2021-02-15 22:48:45 +1100

    created two sides of card

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-// card animation
+// card movement animation
 const card = document.querySelector(".card");
+const cardFront = document.querySelector(".card-front");
+const cardBack = document.querySelector(".card-back");
 const container = document.querySelector(".container");
 
 container.addEventListener("mousemove", (e) => {
@@ -14,6 +16,17 @@ container.addEventListener("mouseenter", (e) => {
 });
 
 container.addEventListener("mouseleave", (e) => {
-  card.style.transform = `rotateY(0deg) rotateX(0deg)`;
   card.style.transition = `all 0.5s ease`;
+  card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+});
+
+// card flip animation
+card.addEventListener("click", (e) => {
+  if (cardBack.style.transform == "rotateY(180deg)") {
+    cardBack.style.transform = `rotateY(0deg)`;
+    cardFront.style.transform = `rotateY(180deg)`;
+  } else {
+    cardBack.style.transform = `rotateY(180deg)`;
+    cardFront.style.transform = `rotateY(0deg)`;
+  }
 });
