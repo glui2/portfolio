bdbc424f968908c60b50ab9dd9863055f1812f40
Gavin <gavin.lui@gmail.com>
2021-02-17 00:49:40 +1100

    finally fixed animations

--- app.js
+++ app.js
@@ -10,13 +10,10 @@ var isMoving = false;
 // card flip animation
 card.addEventListener("click", (e) => {
   isMoving = true;
-  let backRotation = (counter * 180) % 360;
   counter++;
   let frontRotation = (counter * 180) % 360;
   card.style.transition = `all 0.5s ease 0s`;
   card.style.transform = `rotateY(${frontRotation}deg)`;
-  cardFront.style.transform = `rotateY(${frontRotation}deg)`;
-  cardBack.style.transform = `rotateY(${backRotation}deg)`;
   setTimeout(() => {
     isMoving = false;
   }, 500);
@@ -26,27 +23,27 @@ container.addEventListener("mousemove", (e) => {
   if (isMoving) return;
   let xAxis = (window.innerWidth / 2 - e.pageX) / 20;
   let yAxis = (window.innerHeight / 2 - e.pageY) / 20;
-  card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+  container.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
 });
 
 // revert animation transition to none when mouse enters container
 container.addEventListener("mouseenter", (e) => {
   if (isMoving) return;
-  card.style.transition = "none";
+  container.style.transition = "none";
 });
 
 container.addEventListener("mouseleave", (e) => {
   if (isMoving) return;
-  card.style.transition = `all 0.5s ease`;
-  card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+  container.style.transition = `all 0.5s ease`;
+  container.style.transform = `rotateY(0deg) rotateX(0deg)`;
 });
 
-contactIcons.addEventListener("mouseenter", (e) => {
-  if (isMoving) return;
-  contactIcons.style.transform = `translateZ(100px)`;
-});
+// contactIcons.addEventListener("mouseenter", (e) => {
+//   if (isMoving) return;
+//   contactIcons.style.transform = `translateZ(100px)`;
+// });
 
-contactIcons.addEventListener("mouseleave", (e) => {
-  if (isMoving) return;
-  contactIcons.style.transform = `translateZ(0px)`;
-});
+// contactIcons.addEventListener("mouseleave", (e) => {
+//   if (isMoving) return;
+//   contactIcons.style.transform = `translateZ(0px)`;
+// });
