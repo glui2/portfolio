6fd0b6785a0e1018ef09644fdde83f40b37cf6db
Gavin <gavin.lui@gmail.com>
2021-02-17 00:41:35 +1100

    adjusting animations

--- app.js
+++ app.js
@@ -5,28 +5,25 @@ const cardBack = document.querySelector(".card-back");
 const container = document.querySelector(".container");
 const contactIcons = document.querySelector(".icon");
 var counter = 0;
+var isMoving = false;
 
 // card flip animation
 card.addEventListener("click", (e) => {
-  console.log(counter);
+  isMoving = true;
   let backRotation = (counter * 180) % 360;
-  console.log(backRotation);
   counter++;
   let frontRotation = (counter * 180) % 360;
-  console.log(frontRotation);
+  card.style.transition = `all 0.5s ease 0s`;
   card.style.transform = `rotateY(${frontRotation}deg)`;
   cardFront.style.transform = `rotateY(${frontRotation}deg)`;
   cardBack.style.transform = `rotateY(${backRotation}deg)`;
-  //   if (cardBack.style.transform == "rotateY(180deg)") {
-  //     cardBack.style.transform = `rotateY(0deg)`;
-  //     cardFront.style.transform = `rotateY(180deg)`;
-  //   } else {
-  //     cardBack.style.transform = `rotateY(180deg)`;
-  //     cardFront.style.transform = `rotateY(0deg)`;
-  //   }
+  setTimeout(() => {
+    isMoving = false;
+  }, 500);
 });
 
 container.addEventListener("mousemove", (e) => {
+  if (!isMoving) return;
   let xAxis = (window.innerWidth / 2 - e.pageX) / 20;
   let yAxis = (window.innerHeight / 2 - e.pageY) / 20;
   card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
@@ -34,19 +31,22 @@ container.addEventListener("mousemove", (e) => {
 
 // revert animation transition to none when mouse enters container
 container.addEventListener("mouseenter", (e) => {
+  if (!isMoving) return;
   card.style.transition = "none";
 });
 
 container.addEventListener("mouseleave", (e) => {
+  if (!isMoving) return;
   card.style.transition = `all 0.5s ease`;
   card.style.transform = `rotateY(0deg) rotateX(0deg)`;
 });
 
 contactIcons.addEventListener("mouseenter", (e) => {
+  if (!isMoving) return;
   contactIcons.style.transform = `translateZ(100px)`;
-  console.log("test");
 });
 
 contactIcons.addEventListener("mouseleave", (e) => {
+  if (!isMoving) return;
   contactIcons.style.transform = `translateZ(0px)`;
 });
